89fe35dab3f7fa76e995e899b9a5b741df20e50b
roger-dv <roger.dewayne@gmail.com>
2018-12-29 22:09:56 -0800

    function complete implementation for spartan-react-ex example program

--- examples/spartan-react-ex/src/main/java/spartan_react/App.java
+++ examples/spartan-react-ex/src/main/java/spartan_react/App.java
@@ -18,25 +18,30 @@ limitations under the License.
 */
 package spartan_react;
 
-
 import spartan.Spartan;
 import spartan.SpartanBase;
 import spartan.annotations.ChildWorkerCommand;
 import spartan.annotations.SupervisorCommand;
 import spartan.annotations.SupervisorMain;
+import spartan.fstreams.Flow.*;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintStream;
+import java.io.*;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
+import java.util.zip.GZIPInputStream;
+
+import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING;
 
 public class App extends SpartanBase {
-  private static final String clsName = App.class.getName();
   private static final Set<Integer> _pids = ConcurrentHashMap.newKeySet(53);
 
   /**
@@ -110,19 +115,24 @@ public class App extends SpartanBase {
     rspStream.printf("DEBUG: invoked %s.%s(\"%s\")%n", App.class.getName(), methodName, output);
   }
 
+  @SuppressWarnings("unused")
   @SupervisorCommand("MASS_UNCOMPRESS")
   public void massUncompress(String[] args, PrintStream outStream, PrintStream errStream, InputStream inStream) {
     final String methodName = "massUncompress";
     assert args.length > 0;
 
-    try (final PrintStream outS = outStream; final PrintStream errS = errStream; final InputStream inS = inStream) {
-      print_method_call_info(errStream, methodName, args);
+    final String cmd = args[0];
+
+    try (final PrintStream outS = outStream; final PrintStream errS = errStream; final InputStream ignore = inStream) {
+      print_method_call_info(errS, methodName, args);
       if (args.length < 2) {
-        errStream.println("ERROR: expected directory path - insufficient commandline arguments");
+        errS.printf("ERROR: %s: expected directory path - insufficient commandline arguments%n", cmd);
         return;
       }
-      final Path dirPath = validateDirectoryPath(args[1], errS);
+      final Path dirPath = validatePath(cmd, args[1], errS, true);
       if (dirPath == null) return;
+
+      final List<Path> gzFilesList = new ArrayList<>(40);
 
       // obtain list of .gz files to be decompressed
       final EnumSet<FileVisitOption> options = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
@@ -137,28 +147,163 @@ public class App extends SpartanBase {
               @Override
               public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
                 if (file.getFileName().toString().endsWith(".gz")) {
+                  gzFilesList.add(file);
                   outS.println(file);
                 }
                 return FileVisitResult.CONTINUE;
               }
             });
 
-      outS.println("done!");
+      int processedCount = 0;
+
+      if (!gzFilesList.isEmpty()) {
+        processedCount = processGzFiles(cmd, dirPath, gzFilesList, outS, errS);
+      }
+
+      outS.printf("done! %d of %d files processed%n", processedCount, gzFilesList.size());
+    } catch (IOException | InterruptedException | ClassNotFoundException | InvokeCommandException e) {
+      e.printStackTrace(errStream);
+    }
+  }
+
+  private static Path validatePath(String cmd, String pathStr, PrintStream errS, boolean asDirectory) {
+    final Path path = FileSystems.getDefault().getPath(pathStr);
+    if (!Files.exists(path)) {
+      errS.printf("ERROR: %s: specified directory path does not exist: \"%s\"%n", cmd, path);
+      return null;
+    }
+    final String expectation = asDirectory ? "directory" : "file";
+    final Predicate<Path> affirmPath = asDirectory ? Files::isDirectory : Files::isRegularFile;
+    if (!affirmPath.test(path)) {
+      errS.printf("ERROR: %s: specified path is not a %s: \"%s\"%n", cmd, expectation, path);
+      return null;
+    }
+    return path;
+  }
+
+  private static int processGzFiles(String cmd, Path srcDir, List<Path> gzFilesList, PrintStream outS, PrintStream errS)
+        throws IOException, InvokeCommandException, InterruptedException, ClassNotFoundException
+  {
+    final BiFunction<String, String[], Path> getPath = srcDir.getFileSystem()::getPath;
+
+    Subscriber subscriber = null;
+
+    for(final Path inputFile : gzFilesList) {
+      final String inputFileName = inputFile.getFileName().toString();
+      final int lastIndex = inputFileName.lastIndexOf(".gz");
+      final String outputFileName = inputFileName.substring(0, lastIndex);
+      final Path errOutFile = getPath.apply(inputFile.getParent().toString(), new String[]{outputFileName + ".err"});
+      final Path outputFile = getPath.apply(inputFile.getParent().toString(), new String[]{outputFileName});
+      final OutputStream errOutFileStream = Files.newOutputStream(errOutFile, CREATE, TRUNCATE_EXISTING);
+      final OutputStream outputFileStream = Files.newOutputStream(outputFile, CREATE, TRUNCATE_EXISTING);
+
+      final InvokeResponseEx rsp = Spartan.invokeCommandEx("UN_GZIP", inputFile.toString());
+
+      _pids.add(rsp.childPID);
+
+      subscriber = subscriber == null ? spartan.fstreams.Flow.subscribe(rsp) : subscriber.subscribe(rsp);
+      subscriber
+            .onError((errStrm, subscription) -> copyOutToFile(errStrm, errOutFileStream, subscription))
+            .onNext((outStrm,  subscription) -> copyOutToFile(outStrm, outputFileStream, subscription));
+    }
+
+    assert subscriber != null;
+    final FuturesCompletion futures = subscriber.start();
+
+    int count = futures.count();
+    final List<String> pids = new ArrayList<>(count);
+    int i = 0;
+
+    while(count-- > 0) {
+      try {
+        final Integer childPID = futures.take().get();
+        _pids.remove(childPID);
+        pids.add(childPID.toString());
+      } catch (ExecutionException e) {
+        final Throwable cause = e.getCause() != null ? e.getCause() : e;
+        errS.printf("ERROR: %s: exception encountered in sub task:%n", cmd);
+        e.printStackTrace(errS);
+      } catch (InterruptedException e) {
+        errS.printf("WARN: %s: interruption occurred%n", cmd);
+      }
+    }
+
+    outS.printf("INFO: %s: completed child processes:%n%s%n", cmd, String.join(", ", pids.toArray(new String[0])));
+
+    return 0;
+  }
+
+  private static void copyOutToFile(InputStream errStrm, OutputStream errOutFileStream, Subscription subscription) {
+    byte[] buf = new byte[4 * 1024];
+    try (final InputStream errS = errStrm; final OutputStream errOutFS = errOutFileStream) {
+      for(;;) {
+        final int n = errS.read(buf);
+        if (n <= 0) break;
+        errOutFS.write(buf, 0, n);
+      }
+    } catch (IOException e) {
+      subscription.cancel();
+      uncheckedExceptionThrow(e);
+    }
+  }
+
+  /**
+   * Exceptions derived from {@link Throwable}, especially {@link Exception} derived
+   * classes, are re-thrown as unchecked.
+   *
+   * <p/><i>Rethrown exceptions are not wrapped yet the compiler does not detect them as
+   * checked exceptions at compile time so they do not need to be declared in the lexically
+   * containing method signature.</i>
+   *
+   * @param t an exception derived from java.lang.Throwable that will be re-thrown
+   * @return R generic argument allows for use in return statements (though will throw
+   *           exception and thus never actually return)
+   * @throws T the re-thrown exception
+   */
+  @SuppressWarnings("all")
+  private static <T extends Throwable, R> R uncheckedExceptionThrow(Throwable t) throws T { throw (T) t; }
+
+  @SuppressWarnings("unused")
+  @ChildWorkerCommand(cmd = "UN_GZIP", jvmArgs = {"-server", "-Xms64m", "-Xmx128m"})
+  public static void uncompressGzFile(String[] args, PrintStream outStream, PrintStream errStream, InputStream inStream) {
+    final String methodName = "uncompressGzFile";
+    assert args.length > 0;
+
+    final String cmd = args[0];
+
+    try (final PrintStream outS = outStream; final PrintStream errS = errStream; final InputStream inS = inStream) {
+      print_method_call_info(errS, methodName, args);
+      if (args.length < 2) {
+        errS.printf("ERROR: %s: expected .gz file path - insufficient commandline arguments%n", cmd);
+        return;
+      }
+
+      final Path filePath = validatePath(cmd, args[1], errS, false);
+      if (filePath == null) return;
+
+      uncompressGzFileHelper(cmd, filePath, outS, errS, inS);
     } catch (IOException e) {
       e.printStackTrace(errStream);
     }
   }
 
-  private Path validateDirectoryPath(String dirPathStr, PrintStream errS) {
-    final Path dirPath = FileSystems.getDefault().getPath(dirPathStr);
-    if (!Files.exists(dirPath)) {
-      errS.printf("ERROR: specified directory path does not exist: \"%s\"%n", dirPath);
-      return null;
+  private static void uncompressGzFileHelper(String cmd, Path filePath, OutputStream outS, PrintStream errS,
+                                             InputStream inS)
+  {
+    try {
+      final InputStream inputStream = Files.newInputStream(filePath);
+      byte[] buf = new byte[4 + 1024];
+      try (final GZIPInputStream gzipInputStream = new GZIPInputStream(inputStream, 14 * 1024)) {
+        for(;;) {
+          final int n = gzipInputStream.read(buf);
+          if (n <= 0) break;
+          outS.write(buf, 0, n);
+        }
+        outS.flush();
+      }
+    } catch (IOException e) {
+      errS.printf("ERROR: %s: failed un-compressing file: \"%s\"%n", cmd, filePath);
+      e.printStackTrace(errS);
     }
-    if (!Files.isDirectory(dirPath)) {
-      errS.printf("ERROR: specified path is not a directory: \"%s\"%n", dirPath);
-      return null;
-    }
-    return dirPath;
   }
 }
